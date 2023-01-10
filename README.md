7688d70b416684fe03e0baeeaad8bd3c48ec0a6c
Tanish <tanishsingh.tak9829@gmail.com>
2023-01-10 18:59:00 +0530

    Add solution for problem `C - Interesting Sequence` Link: https://codeforces.com/contest/1775/problem/C Tags: bitmasks, math, *1600 Ref: https://codeforces.com/contest/1775/submission/188754275

--- README.md
+++ README.md
@@ -12,9 +12,10 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-6 | [B - Stand-up Comedian](https://codeforces.com/contest/1792/problem/B) | [GNU C++14](./codeforces/1792/B.cpp) | `greedy` `math` `*1200` | Jan/24/2023 22:20 | 
-5 | [C - Equal Frequencies](https://codeforces.com/contest/1782/problem/C) | [GNU C++14](./codeforces/1782/C.cpp) | `brute force` `constructive algorithms` `greedy` `implementation` `sortings` `strings` `*1600` | Jan/18/2023 19:09 | 
-4 | [B - Going to the Cinema](https://codeforces.com/contest/1782/problem/B) | [GNU C++14](./codeforces/1782/B.cpp) | `brute force` `greedy` `sortings` `*1000` | Jan/15/2023 18:12 | 
-3 | [A - Parallel Projection](https://codeforces.com/contest/1782/problem/A) | [GNU C++14](./codeforces/1782/A.cpp) | `geometry` `greedy` `math` `*800` | Jan/15/2023 17:43 | 
-2 | [A - Make it Beautiful](https://codeforces.com/contest/1783/problem/A) | [GNU C++14](./codeforces/1783/A.cpp) | `constructive algorithms` `math` `sortings` `*800` | Jan/15/2023 17:30 | 
-1 | [B - Gardener and the Array](https://codeforces.com/contest/1775/problem/B) | [GNU C++14](./codeforces/1775/B.cpp) | `bitmasks` `constructive algorithms` `*1300` | Jan/11/2023 00:13 | 
+7 | [B - Stand-up Comedian](https://codeforces.com/contest/1792/problem/B) | [GNU C++14](./codeforces/1792/B.cpp) | `greedy` `math` `*1200` | Jan/24/2023 22:20 | 
+6 | [C - Equal Frequencies](https://codeforces.com/contest/1782/problem/C) | [GNU C++14](./codeforces/1782/C.cpp) | `brute force` `constructive algorithms` `greedy` `implementation` `sortings` `strings` `*1600` | Jan/18/2023 19:09 | 
+5 | [B - Going to the Cinema](https://codeforces.com/contest/1782/problem/B) | [GNU C++14](./codeforces/1782/B.cpp) | `brute force` `greedy` `sortings` `*1000` | Jan/15/2023 18:12 | 
+4 | [A - Parallel Projection](https://codeforces.com/contest/1782/problem/A) | [GNU C++14](./codeforces/1782/A.cpp) | `geometry` `greedy` `math` `*800` | Jan/15/2023 17:43 | 
+3 | [A - Make it Beautiful](https://codeforces.com/contest/1783/problem/A) | [GNU C++14](./codeforces/1783/A.cpp) | `constructive algorithms` `math` `sortings` `*800` | Jan/15/2023 17:30 | 
+2 | [B - Gardener and the Array](https://codeforces.com/contest/1775/problem/B) | [GNU C++14](./codeforces/1775/B.cpp) | `bitmasks` `constructive algorithms` `*1300` | Jan/11/2023 00:13 | 
+1 | [C - Interesting Sequence](https://codeforces.com/contest/1775/problem/C) | [GNU C++14](./codeforces/1775/C.cpp) | `bitmasks` `math` `*1600` | Jan/10/2023 18:59 | 
---
+++ codeforces/1775/C.cpp
@@ -0,0 +1,238 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Fast_io ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define endl '\n'     //Comment out if interactive
+#define int long long // comment if Space limit exceeds
+const int N = 1e5+10; //change to 32000 if TLE happens for prime factors
+const int mod = 1e9 +7; 
+int isprime[N]; // for sieve
+vector<int> primes; // for storing primes
+vector<int> vis(N,0); // visited array
+int parent[N]; // for DSU change it accordingly for something else
+int size[N]; // for DSU change it accordingly for something else
+map<int,int> size_map; // for kruskal 
+int fact[N]; // factorial array
+
+
+
+// binary multiply with mod
+int binMultiply(long long a, long long b,long long m){
+    int ans =0;
+    while(b){
+        if(b&1){
+            ans = (ans + a)%m;
+        }
+        a = (a +a)%m;
+        b >>= 1;
+    }
+    return ans;
+}
+
+//binary exponentiation with mod
+int binexp(long long a, long long b, long long m){
+    int ans =1;
+    while(b){
+        if(b&1){
+            ans = binMultiply(ans,a,m)%m;
+        }
+        a = binMultiply(a,a,m)%m;
+        b >>= 1;
+    }
+    return ans;
+}
+
+//Factorial Pre-compute
+void factorial(long long m){
+    fact[0] = 1;
+    for(int i = 0; i<N; i++){
+        fact[i] = (fact[i-1]*1LL*i)%m;
+    }
+}
+
+// Modulo multiplicative inverse MMI
+// (A^-1)%m = (A^(m-2))%m
+int moduloinverse(long long x , long long m){
+    return binexp(x,m-2,m);
+}
+
+
+int nCr(long long n, long long r, long long m){
+    int num = fact[n];//numerator
+    int den = (fact[n-r]*1LL*fact[r])%m; // denominator
+    int ans = (num*1LL*moduloinverse(den,m))%m; // using MMI for denominator
+    return ans;
+}
+
+
+// Sieve Algorithm
+void sieve(){
+    for(int  i = 0; i<N; i++){
+        isprime[i] = i;
+    }
+    for(int i=2;i<N; i++){
+        if(isprime[i] != i){
+            continue;}
+        for(int j = 2*i ; j<N; j+=i){
+            isprime[j] = min(isprime[j], i);
+        }
+        primes.push_back(i);
+    }
+}
+
+
+// Function for prime factors
+vector<int> getPrimeFactors(int a) {
+    vector<int> f;
+    while(a>1){
+        if(f.empty()||f.back() != isprime[a])
+            f.push_back(isprime[a]);
+        a/=isprime[a];
+    }
+    return f;
+}
+
+//DSU begin
+void make(int v){
+    parent[v] =v;
+  size[v] = 1;
+}
+
+int find(int v){
+    if(parent[v] == v){return v;}
+    return parent[v] = find(parent[v]);
+}
+
+void Union(int a, int b){
+  a = find(a);
+  b = find(b);
+    if(a!=b){
+    if(size[a]<size[b]){
+      swap(a,b);
+    }
+        parent[b] = a;
+    size[a]+=size[b];
+    }
+}
+
+//merge used for kruskal
+void merge(int a, int b){
+    size_map[size[a]]--;
+    if(size_map[size[a]] == 0){
+        size_map.erase(size[a]);
+    }
+    size_map[size[b]]--;
+    if(size_map[size[b]] == 0){
+        size_map.erase(size[b]);
+    }
+    size_map[size[a] + size[b]]++;
+}
+// DSU end
+
+//subset generation using bitMasking
+vector<vector<int>> subsets(vector<int>& nums ){
+    int n = nums.size();
+    int subset_ct = 1<<n;
+    vector<vector<int>> subsets_list;
+    for(int mask = 0; mask < subset_ct; ++mask){
+        vector<int> subset;
+        for(int i = 0; i < n; ++i){
+            if((mask & (1<<i)) != 0){
+                subset.push_back(nums[i]);
+            }
+        }
+        subsets_list.push_back(subset);
+    }
+    return subsets_list;
+} 
+
+//general DFS
+void DFS(int vertex, vector<vector<int>> graph){
+    //1. take action on vertex after entering vertex
+    for(int child : graph[vertex]){
+        //2. take action on child node before entering child node
+        DFS(child, graph);
+        //3. take action on child node after exiting child node
+    }
+    //4. take action on vertex before exiting vertex
+}
+
+/////----------------------------Main Function---------------------------------/////
+
+vector<int> decToBinary(int &n)
+{
+    vector<int> binaryNum(64,0);
+
+    int i = 0;
+    while (n > 0) {
+        binaryNum[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+    return binaryNum;
+}
+signed main() {
+
+    Fast_io;
+    // cout.precision(30);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        int t1 = n;
+        int t2 = x;
+        vector<int> n1 = decToBinary(n);
+        vector<int> n2 = decToBinary(x);
+        bool f =1;
+        bool f2 = 1;
+        vector<int> one_0;
+        int one_1;
+        for(int i=0;i<64;i++){
+            if(n1[i] == 0 && n2[i] == 1){
+                f =0;
+                break;
+            }
+            else if(n1[i] == 1 && n2[i] ==1){
+                f2 =0;
+                one_1 = i;
+            }
+            else if((n1[i] == 1 && n2[i]==0) ){
+                if(f2==0){
+                f = 0;
+                break;
+                }
+                one_0.push_back(i);
+            }
+        }
+        if(f==0){
+            cout<<-1<<endl;
+        }
+        else if(t1==t2){
+            cout<<t1<<endl;
+        }
+        else{
+            int j = one_0[one_0.size()-1];
+            if(n1[j+1] ==1 && n2[j+1]==1){
+                cout<<-1<<endl;
+            }
+            else{
+                for(int i =0; i<=j; i++){
+                n1[i]=0;
+            }
+            n1[j+1]=1;
+            int ans = 0;
+            for(int i = 0; i<64; i++){
+                if(n1[i] == 1){
+                    ans+=pow(2,i);
+                }
+            }
+            cout<<ans<<endl;
+            }
+            
+        }
+
+    }
+
+    return 0;
+}
