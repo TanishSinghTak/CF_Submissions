67c3912ec61595c2487566cfb12ad00661499f57
Tanish <tanishsingh.tak9829@gmail.com>
2023-01-15 18:12:00 +0530

    Add solution for problem `B - Going to the Cinema` Link: https://codeforces.com/contest/1782/problem/B Tags: brute force, greedy, sortings, *1000 Ref: https://codeforces.com/contest/1782/submission/189321446

--- README.md
+++ README.md
@@ -12,5 +12,6 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-2 | [B - Stand-up Comedian](https://codeforces.com/contest/1792/problem/B) | [GNU C++14](./codeforces/1792/B.cpp) | `greedy` `math` `*1200` | Jan/24/2023 22:20 | 
-1 | [C - Equal Frequencies](https://codeforces.com/contest/1782/problem/C) | [GNU C++14](./codeforces/1782/C.cpp) | `brute force` `constructive algorithms` `greedy` `implementation` `sortings` `strings` `*1600` | Jan/18/2023 19:09 | 
+3 | [B - Stand-up Comedian](https://codeforces.com/contest/1792/problem/B) | [GNU C++14](./codeforces/1792/B.cpp) | `greedy` `math` `*1200` | Jan/24/2023 22:20 | 
+2 | [C - Equal Frequencies](https://codeforces.com/contest/1782/problem/C) | [GNU C++14](./codeforces/1782/C.cpp) | `brute force` `constructive algorithms` `greedy` `implementation` `sortings` `strings` `*1600` | Jan/18/2023 19:09 | 
+1 | [B - Going to the Cinema](https://codeforces.com/contest/1782/problem/B) | [GNU C++14](./codeforces/1782/B.cpp) | `brute force` `greedy` `sortings` `*1000` | Jan/15/2023 18:12 | 
---
+++ codeforces/1782/B.cpp
@@ -0,0 +1,214 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define Fast_io ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define endl '\n'     //Comment out if interactive
+#define int long long // comment if Space limit exceeds
+const int N = 1e5+10; //change to 32000 if TLE happens for prime factors
+const int mod = 1e9 +7; 
+int isprime[N]; // for sieve
+vector<int> primes; // for storing primes
+vector<int> vis(N,0); // visited array
+int parent[N]; // for DSU change it accordingly for something else
+int size[N]; // for DSU change it accordingly for something else
+map<int,int> size_map; // for kruskal 
+int fact[N]; // factorial array
+
+
+
+// binary multiply with mod
+int binMultiply(long long a, long long b,long long m){
+    int ans =0;
+    while(b){
+        if(b&1){
+            ans = (ans + a)%m;
+        }
+        a = (a +a)%m;
+        b >>= 1;
+    }
+    return ans;
+}
+
+//binary exponentiation with mod
+int binexp(long long a, long long b, long long m){
+    int ans =1;
+    while(b){
+        if(b&1){
+            ans = binMultiply(ans,a,m)%m;
+        }
+        a = binMultiply(a,a,m)%m;
+        b >>= 1;
+    }
+    return ans;
+}
+
+//Factorial Pre-compute
+void factorial(long long m){
+    fact[0] = 1;
+    for(int i = 0; i<N; i++){
+        fact[i] = (fact[i-1]*1LL*i)%m;
+    }
+}
+
+// Modulo multiplicative inverse MMI
+// (A^-1)%m = (A^(m-2))%m
+int moduloinverse(long long x , long long m){
+    return binexp(x,m-2,m);
+}
+
+
+int nCr(long long n, long long r, long long m){
+    int num = fact[n];//numerator
+    int den = (fact[n-r]*1LL*fact[r])%m; // denominator
+    int ans = (num*1LL*moduloinverse(den,m))%m; // using MMI for denominator
+    return ans;
+}
+
+
+// Sieve Algorithm
+void sieve(){
+    for(int  i = 0; i<N; i++){
+        isprime[i] = i;
+    }
+    for(int i=2;i<N; i++){
+        if(isprime[i] != i){
+            continue;}
+        for(int j = 2*i ; j<N; j+=i){
+            isprime[j] = min(isprime[j], i);
+        }
+        primes.push_back(i);
+    }
+}
+
+
+// Function for prime factors
+vector<int> getPrimeFactors(int a) {
+    vector<int> f;
+    while(a>1){
+        if(f.empty()||f.back() != isprime[a])
+            f.push_back(isprime[a]);
+        a/=isprime[a];
+    }
+    return f;
+}
+
+//DSU begin
+void make(int v){
+    parent[v] =v;
+  size[v] = 1;
+}
+
+int find(int v){
+    if(parent[v] == v){return v;}
+    return parent[v] = find(parent[v]);
+}
+
+void Union(int a, int b){
+  a = find(a);
+  b = find(b);
+    if(a!=b){
+    if(size[a]<size[b]){
+      swap(a,b);
+    }
+        parent[b] = a;
+    size[a]+=size[b];
+    }
+}
+
+//merge used for kruskal
+void merge(int a, int b){
+    size_map[size[a]]--;
+    if(size_map[size[a]] == 0){
+        size_map.erase(size[a]);
+    }
+    size_map[size[b]]--;
+    if(size_map[size[b]] == 0){
+        size_map.erase(size[b]);
+    }
+    size_map[size[a] + size[b]]++;
+}
+// DSU end
+
+//subset generation using bitMasking
+vector<vector<int>> subsets(vector<int>& nums ){
+    int n = nums.size();
+    int subset_ct = 1<<n;
+    vector<vector<int>> subsets_list;
+    for(int mask = 0; mask < subset_ct; ++mask){
+        vector<int> subset;
+        for(int i = 0; i < n; ++i){
+            if((mask & (1<<i)) != 0){
+                subset.push_back(nums[i]);
+            }
+        }
+        subsets_list.push_back(subset);
+    }
+    return subsets_list;
+} 
+
+//general DFS
+void DFS(int vertex, vector<vector<int>> graph){
+    //1. take action on vertex after entering vertex
+    for(int child : graph[vertex]){
+        //2. take action on child node before entering child node
+        DFS(child, graph);
+        //3. take action on child node after exiting child node
+    }
+    //4. take action on vertex before exiting vertex
+}
+
+/////----------------------------Main Function---------------------------------/////
+
+
+signed main() {
+
+    Fast_io;
+    // cout.precision(30);
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr;
+        for(int i =0; i<n; i++){
+            int x;
+            cin>>x;
+            arr.push_back(x);
+        }
+        sort(arr.begin(), arr.end());
+        int total =0;
+        int ans;
+
+        if(arr[0] == 0){
+            ans = 0;
+        }
+        else{
+            ans = 1;
+        }
+        for(int i =0; i<n; i++){
+            // if(arr[i]<=total){
+            //     int x =1;
+            //     while(arr[i+1]==arr[i] && i<n){
+            //         i++;
+            //         x++;
+            //     }
+            //     total+=x;
+            //     if(i<n && arr[i+1]>total ){
+            //         ans++;
+            //     }
+            // }
+            // else{
+            //     total++;
+            // }
+            if(arr[i]<=i){
+                while(i<n && arr[i]<=i){
+                    i++;
+                }
+                ans++;
+            }
+        }
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
